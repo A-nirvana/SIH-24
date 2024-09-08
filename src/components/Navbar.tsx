d9178ab745038fad9c597626a61da3d7d5c99b7f
Pixel Hermit <145558376+A-nirvana@users.noreply.github.com>
2024-09-08 16:17:53 +0530

    Landing page started (#2)
    Co-authored-by: A-nirvana <anirban_ug_23@gmail.com>

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,17 @@
+"use client";
 import Link from "next/link";
+import Button from "./ui/button";
 
 export default function Navbar() {
   return (
-    <nav className="w-full h-[4rem] bg-[#003d72] flex justify-between">
+    <nav className="w-full h-[4rem] top-0 px-8 py-4 bg-[#222831] flex justify-between absolute">
       <div>Logo</div>
       <div className="flex justify-around gap-4">
         <Link href="/">FAQ's</Link>
         <Link href="/">Announcement</Link>
         <Link href="/">HelpDesk</Link>
       </div>
+      <div><Button onClick={()=>{}}>Register</Button></div>
     </nav>
   );
 }

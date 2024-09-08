d97ce3ea1f4ccba5b18591d4d956104ef1ba7bfa
A-nirvana <anirban_ug_23@gmail.com>
2024-09-08 16:16:52 +0530

    Landing page started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,22 @@
 "use client";
+import { Jost } from "next/font/google";
 
-import DragAndDropImageUploader from "@/components/DragDrop";
+const jost = Jost({ subsets: ["latin"] });
+
 export default function Home() {
   const handleImageUpload = (file: File) => {
     console.log("Image uploaded:", file);
   };
   return (
-    <section className="h-[100vh] w-full bg-gradient-to-b from-customBlueDark via-customBlueLight to-customGray"></section>
+    <main className={"h-[100vh] w-full bg-[#222831] flex px-16 " + jost.className}>
+      <section className="flex flex-col items-center justify-center ml-20 h-full w-1/2">
+      <p className=" mt-12 text-6xl font-semibold">
+      Lorem ipsum dolor sit amet consectetur
+      </p>
+      <p className="mt-4 text-2xl mr-12">lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia .</p>
+      </section>
+      {/* The image is not good quality ask DEvanuj to share original */}
+      <section><img src="/landingGirl.svg" className="w-96"/></section>
+    </main>
   );
 }

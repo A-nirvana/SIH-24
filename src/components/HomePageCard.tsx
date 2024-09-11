c521eeb3ef096fadf756a8d42b74896cd1d8d851
karock <148215838+hirakRajbonshi@users.noreply.github.com>
2024-09-11 17:53:45 +0530

    landing page almost done (#3)
    Complete hole bhabi nathakiba

---
+++ src/components/HomePageCard.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+interface inputProps {
+  svg: string;
+  text: string;
+}
+export default function HomePageCard(props: inputProps) {
+  return (
+    <div className="h-[12rem] w-[12rem] hover:border hover:border-[#6B58CA] hover:border-spacing-4 rounded-2xl flex flex-col items-center justify-center">
+      <Image src={props.svg} width={40} height={40} alt="svg" />
+      <h6 className="text-black text-xl">{props.text}</h6>
+    </div>
+  );
+}

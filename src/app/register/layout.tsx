f5322a5fea74625125fd96ba334af275f4d470b8
karock <148215838+hirakRajbonshi@users.noreply.github.com>
2024-09-12 11:41:13 +0530

    added register route (#4)
    * landing page almost done
    
    * added register route

---
+++ src/app/register/layout.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <div className="w-full h-[4rem]"></div>
+      <section className="w-full h-auto bg-white text-black px-24 py-20 flex flex-col gap-4">
+        <h1 className="text-5xl font-semibold">Register Now</h1>
+        <h6 className="text-sm text-[#696B6E]">
+          Please fill in the form below. We'll contact you as soon as possible.
+        </h6>
+        <div className="w-full h-auto ">
+          <div className="full flex justify-evenly bg-[#93a1ad57] text-[#3A3A3A] p-3 rounded-t-xl">
+            <Link href="/register/personal-info">Personal Information</Link>
+            <Link href="/register/educational-details">Education Details</Link>
+            <Link href="/register/documents">Documents upload</Link>
+            <Link href="/register/bank-details">Bank Details</Link>
+          </div>
+          <div className="w-full min-h-[10rem] h-auto rounded-b-xl bg-[#E8E8E8]">
+            {children}
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
